fc6df2158969eda26ca01990b1ea0b8b7510d967
Hernan Oliva <hhernanoliva@gmail.com>
2022-05-26 12:17:58 -0300

    update

---
+++ src/lib/button.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let backgroundColor;
+
+</script>
+
+<button style:background-color={backgroundColor}> <slot /> </button>
+
+<style>
+    button{
+        padding: 10px;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../app.scss";
+    import Button from "$lib/button.svelte"
+    
+</script>
+
+<h1  class="text-9xl titulo">eNube</h1>
+
+
+<Button backgroundColor={"yellow"}>Lucas</Button>
+
+<Button backgroundColor={"#70bc9a"}>Hernan</Button>
+
+<a href="">DIRECCION</a>
+<style lang="scss">
+
+</style>    
