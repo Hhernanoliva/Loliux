97bbd837bb038b9fc807fdb595e911b1981c4c45
Hernan Oliva <hhernanoliva@gmail.com>
2022-06-17 17:58:00 -0300

    update

--- src/routes/project/project.svelte
+++ src/routes/project/project.svelte
@@ -5,7 +5,7 @@
 
 </script>
 
-<div id="main" class="flex flex-wrap flex-row justify-center justify-items-center w-full"> 
+<div id="main" class="flex flex-wrap flex-row justify-center justify-items-center w-full">
 
     {#each projectDB as { title, copy, time, src, imgWidth, imgHeight } }
         <div class="shadow-md drop-shadow-md rounded-2xl max-w-xs p-2 mb-10 md:mb-20 mx-0 md:mx-12 bg-white">
